f2524ba7a82beb9bedf76cc1087e5d7c89b29f21
souvik <souvikavro00.sa@gmail.com>
2023-08-02 23:32:52 +0600

    Final assognment0 Done

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 void main() =>runApp(MaterialApp(
   home:Scaffold(
     appBar:AppBar(
-      title: Text('Hello World'),
+      title: Text('MY FIRST PROJECT'),
+      centerTitle: true,
     ),
+     body: Center(
+       child: Text('HELLO WORLD'),
+     ),
   ),
 ));
 
